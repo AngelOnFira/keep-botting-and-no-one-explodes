ce140dbf388a84df64117ffd9081e389c474bdd9
Forest <wizcardforest@gmail.com>
2020-01-11 07:27:53 -0500

    Fixed basic testing

---
+++ LOG.md
@@ -0,0 +1,9 @@
+## Jan 9: Starting modules
+
+I started by trying to implement a trivial module's logic, simple wires. I had to think of a way to structure files, so that any module can be generically called later. I'm going to put all of the modules into a module folder, but keep each separate in its own file. That way, one can be imported as follows:
+
+`from modules.wires-simple import WiresSimple`
+
+Next, I got testing set up. All the modules should be developed with Test Driven Development practices. Each module has its own discrete and explicit rules, and so creating test cases is pretty defined. Also, a solid testing suite will allow us to prevent regression on modules. Once we get to parsing the bomb with OpenCV, we should be confident that we don't have to debug modules there.
+
+For testing, we are using pytest to write the tests, and Github Actions to test them before they are merged.
--- tests/test_wires_simple.py
+++ tests/test_wires_simple.py
@@ -1,84 +1,84 @@
-from modules.simple-wires import SimpleWires
 import pytest
+from modules.wires_simple import WiresSimple
 
 
 def test_3_wires(capsys):
-    first_condition = new SimpleWires([
+    first_condition = WiresSimple((0, 0), "ABC123")
+    first_condition.solution([
         'blue',
         'blue',
         'blue',
         '',
         '',
         '',
-    ], (0, 0))
-    first_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 2 in position 2\n"
 
-    second_condition = new SimpleWires([
+    second_condition = WiresSimple((0, 0), "ABC123")
+    second_condition.solution([
         'blue',
         '',
         'red',
         '',
         'white',
         '',
-    ], (0, 0))
-    second_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 3 in position 6\n"
 
-    third_condition = new SimpleWires([
+    third_condition = WiresSimple((0, 0), "ABC123")
+    third_condition.solution([
         'blue',
         'red',
         '',
         'blue',
         '',
         '',
-    ], (0, 0))
-    third_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 3 in position 4\n"
 
-    fourth_condition = new SimpleWires([
+    fourth_condition = WiresSimple((0, 0), "ABC123")
+    fourth_condition.solution([
         'blue',
         'red',
         '',
         '',
         'black',
         '',
-    ], (0, 0))
-    fourth_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 3 in position 5\n"
 
-    mixed_condition = new SimpleWires([
+    mixed_condition = WiresSimple((0, 0), "ABC123")
+    mixed_condition.solution([
         'blue',
         'blue',
         'white',
         '',
         'black',
         'blue',
-    ], (0, 0))
-    mixed_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 4 in position 5\n"
 
 
-def test_4_wires():
-    first_condition = new SimpleWires([
+def test_4_wires(capsys):
+    first_condition = WiresSimple((0, 0), "ABC123")
+    first_condition.solution([
         'blue',
         'red',
         '',
         '',
         'black',
         '',
-    ], (0, 0))
-    first_condition.solution()
+    ])
 
     captured = capsys.readouterr()
     assert captured.out == "Simple Wires: I cut wire 3 in position 5\n"
