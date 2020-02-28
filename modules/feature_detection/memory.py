3b281d60964052ca9666e03850af1f3dba60a8f2
Aaron Ramos-Lazette <aaronramoslazette@cmail.carleton.ca>
2020-02-27 20:41:56 -0500

    Correctly determining display and buttons.
    Still needs to *report* the display back to the caller.
    Images need to be moved around.
    Test script needs to be integrated into pytest.

--- modules/feature_detection/memory.py
+++ modules/feature_detection/memory.py
@@ -1,17 +1,20 @@
 import cv2 as cv
 import pytesseract
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
 # can this be removed?
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+DISPLAYS_DIRECTORY = "./displays"
 
 class ColourRange:
 
 	def __init__(self, min, max):
 		self.min = np.array(min, np.uint8)
-		self.max = np.array(max, np.uint8)	
+		self.max = np.array(max, np.uint8)
 
-WHITE = ColourRange([0,0,215], [10,40,255])
 BROWN = ColourRange([15,65,38], [25,83,55])
 
 # as provided by opencv-python's documentation
@@ -37,33 +40,89 @@ def harrisCorner(img):
 	res = np.int0(res)
 	img[res[:,1],res[:,0]]=[0,0,255]
 	img[res[:,3],res[:,2]] = [0,255,0]
-	
+
 	cv.imshow('harris', img)
 	return img
 
 def extract_text_helper(img, hsv, c_range, img_out=None):
 	mask = cv.inRange(hsv, c_range.min, c_range.max)
-	#img = cv.medianBlur(img,5)
-	#img = cv.GaussianBlur(img, (5, 5), 0)
 	img = cv.bitwise_and(img, img, mask=mask)
-	#img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-	#img = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
 	img = cv.bitwise_not(img)
-	#img = cv.GaussianBlur(img, (5, 5), 0)
-	#img = cv.blur(img, (5, 5))
 
 	if img_out is not None:
 		cv.imwrite(img_out + '_mask.png', mask)
 		cv.imwrite(img_out + '_feats.png', img)
 	return pytesseract.image_to_string(img)
-	
+
+# borrowed from AngelOnFira's working branch
+def matchImages(img1, img2, numPoints):
+    # TODO when loading images, throw out
+    # any points that arent in ideal place
+
+    orb = cv.ORB_create()
+
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+
+    bf = cv.BFMatcher_create(cv.NORM_HAMMING, crossCheck=True)
+
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    numPoints = min(numPoints, len(matches))
+
+    # src_pts = np.float32([kp1[m.queryIdx].pt for m in range(10)]).reshape(-1, 1, 2)
+    dst_pts = np.float32(
+        [kp2[matches[i].trainIdx].pt for i in range(numPoints)]).reshape(-1, 1, 2)
+
+    output = ([], matches[:numPoints])
+    for i in range(numPoints):
+        output[0].append(dst_pts[i][0])
+
+    img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
+    #cv.imwrite("tes.png", img3)
+
+    return output
+
+# borrowed from AngelOnFira's working branch
+def detectDisplay(displayImg):
+    # Have a module image passed in, figure out which one it is
+
+    # Load array of images
+    displayReferences = []
+
+    for f in listdir(DISPLAYS_DIRECTORY):
+        if isfile(join(DISPLAYS_DIRECTORY, f)):
+            displayReferences += [cv.imread(join(DISPLAYS_DIRECTORY, f), 0)]
+
+    # Check displayImg against each
+    displayMatches = []
+
+    for reference in displayReferences:
+        # [[reference, sumDistance], ...]
+        matches = matchImages(displayImg, reference, 10)
+        displayMatches.append(
+            (reference, sum([m.distance for m in matches[1]])))
+
+    # Return most likely image
+    return min(displayMatches, key=lambda match: match[1])[0]
+
 def extract_text(img, img_out=None):
 	#img = harrisCorner(img) # odd, but it currently only seems to register the buttons after applying Harris Corner
 	hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+	#if img_out is not None:
+	#	img_out += '_display'
+	#display_text = extract_text_helper(img, hsv_img, WHITE, img_out)
 	if img_out is not None:
-		img_out += '_display'
-	display_text = extract_text_helper(img, hsv_img, WHITE, img_out)
+		img_out += '_display.png'
+		cv.imwrite(img_out, detectDisplay(img))
+
+	# To do: replace with actual report from detectDisplay
+	display_text = ""
+
 	if img_out is not None:
 		img_out += '_buttons'
+
 	buttons_text = extract_text_helper(img, hsv_img, BROWN, img_out)
+
 	return (display_text, buttons_text)
--- modules/feature_detection/test_memory_detection.py
+++ modules/feature_detection/test_memory_detection.py
@@ -2,42 +2,31 @@
 # I am having issues with my programming environment that
 # is preventing tesseract from being loaded by pytest
 
-#import pytest
-#import modules.feature_detection.memory as memory
 import memory
 import cv2 as cv
 import os
 
-#module = 'Memory'
-
 def test1():
-	img = cv.imread('./memory.png')
-	d, b = memory.extract_text(img, '1')
+	img = cv.imread('./1_mod.png')
+	d, b = memory.extract_text(img)
 	print("Display: " + d)
 	print("Buttons: " + b)
-	return d == '1' and b == '3124'
+	return d == '1' and b == '4123'
 
 def test2():
 	img = cv.imread('./2_mod.png')
-	d, b = memory.extract_text(img, '2')
+	d, b = memory.extract_text(img)
 	print("Display: " + d)
 	print("Buttons: " + b)
 	return d == '2' and b == '2413'
-	
+
 def test3():
 	img = cv.imread('./3_mod.png')
-	d, b = memory.extract_text(img, '3')
+	d, b = memory.extract_text(img)
 	print("Display: " + d)
 	print("Buttons: " + b)
 	return d == '3' and b == '4213'
-	
-def get2and4():
-	img = cv.imread('./2.png')
-	memory.extract_text(img)
-	img = cv.imread('./4.png')
-	memory.extract_text(img)
 
-#get2and4()
 one = test1()
 two = test2()
 three = test3()
