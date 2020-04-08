3cfe60f7cb589d1b541286b0153b5a936ce7b06c
nemzutkovic <nemzutkovic@icloud.com>
2020-04-07 20:44:21 -0400

    Finished button colour detection for button module
    - Added modularity to colour detection for code reuse
    - Changed naming convention of some images
    - Added further tests for detecting button colour
    - Missing the black button colour boundaries for detection, and missing an image for the black button. Hard to find

--- modules/feature_detection/button.py
+++ modules/feature_detection/button.py
@@ -2,21 +2,20 @@ import cv2
 from os import listdir
 from os.path import isfile, join
 
-def extractStripColour(img, img_out=None):
+RESIZED_IMAGE_WIDTH = 300
 
-	strip_colour = "OTHER"
-	r = 300.0 / img.shape[1]
-	dim = (300, int(img.shape[0] * r))
-	resized_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-	cropped_img = resized_img[136:256, 240:266]
+def extractColour(img, img_out=None):
 
-	# Colour boundaries may need tweaking to improve success rate
+	colour = "UNKNOWN"
+
+	# Colour boundaries may need tweaking to improve success rate for extracting colour
+	red_boundaries    = [[22, 22, 157], [64, 52, 255]]
 	blue_boundaries   = [[86, 31, 4], [255, 120, 50]]
-	yellow_boundaries = [[0, 170, 200], [15, 255, 255]]
-	white_boundaries  = [[180,180,180],[255,255,255]]
+	yellow_boundaries = [[0, 150, 200], [40, 255, 255]]
+	white_boundaries  = [[180, 180, 180],[255, 255, 255]]
 
-	y = cropped_img.shape[0]
-	x = cropped_img.shape[1]
+	y = img.shape[0]
+	x = img.shape[1]
 
 	strip_colour_index0 = 0
 	strip_colour_index1 = 0
@@ -25,27 +24,52 @@ def extractStripColour(img, img_out=None):
 
 	for y in range(0, y):
 		for x in range(0, x):
-			pixel_colour = cropped_img[y,x]
+			pixel_colour = img[y,x]
 			strip_colour_index0 += pixel_colour[0]
 			strip_colour_index1 += pixel_colour[1]
 			strip_colour_index2 += pixel_colour[2]
 			pixelcount += 1
 
 	average_strip_colour = [round(strip_colour_index0/pixelcount), round(strip_colour_index1/pixelcount), round(strip_colour_index2/pixelcount)]
-	#print(average_strip_colour) -> Useful for debugging colour boundaries
+	#print(average_strip_colour) # -> Useful for debugging colour boundaries
 
-	if extractStripColourHelper(average_strip_colour, white_boundaries):
-		return "WHITE"
+	if extractColourHelper(average_strip_colour, white_boundaries):
+		colour = "WHITE"
+		return colour
 
-	if extractStripColourHelper(average_strip_colour, yellow_boundaries):
-		return "YELLOW"
+	if extractColourHelper(average_strip_colour, yellow_boundaries):
+		colour = "YELLOW"
+		return colour
 
-	if extractStripColourHelper(average_strip_colour, blue_boundaries):
-		return "BLUE"
+	if extractColourHelper(average_strip_colour, blue_boundaries):
+		colour = "BLUE"
+		return colour
 
-	return strip_colour
+	if extractColourHelper(average_strip_colour, red_boundaries):
+		colour = "RED"
+		return colour
 
-def extractStripColourHelper(avg_strip_color, colour_boundaries):
+	return colour
+
+def extractColourHelper(avg_strip_color, colour_boundaries):
 	if avg_strip_color[0] >= colour_boundaries[0][0] and avg_strip_color[1] >= colour_boundaries[0][1] and  avg_strip_color[2] >= colour_boundaries[0][2]:
 		if avg_strip_color[0] <= colour_boundaries[1][0] and avg_strip_color[1] <= colour_boundaries[1][1] and avg_strip_color[2] <= colour_boundaries[1][2]:
 			return True
+
+def resizeImage(img, image_width, img_out=None):
+	new_ratio = image_width / img.shape[1]
+	new_dimensions = (image_width, int(img.shape[0] * new_ratio)) # New image height computed here
+	resized_img = cv2.resize(img, new_dimensions, interpolation = cv2.INTER_AREA)
+	return resized_img
+
+def extractStripColour(img, img_out=None):
+	resized_img = resizeImage(img, RESIZED_IMAGE_WIDTH)
+	cropped_img = resized_img[136:256, 240:266]
+	strip_colour = extractColour(cropped_img)
+	return strip_colour
+
+def extractButtonColour(img, img_out=None):
+	resized_img = resizeImage(img, RESIZED_IMAGE_WIDTH)
+	cropped_img = resized_img[120:140, 80:160]
+	button_colour = extractColour(cropped_img)
+	return button_colour
--- tests/test_button_detection.py
+++ tests/test_button_detection.py
@@ -8,93 +8,165 @@ MODULE_IMAGES_DIR = "../images/modules/feature_detection/button/"
 
 ### LARGE RESOLUTION TESTS ####
 def test_strip_blue_largeres():
-	print("Testing blue strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'Press-Red-Blue-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'LargeRes-Red-Press-Blue-Strip.png')
+
+	print("Testing large resolution blue strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "BLUE"
 
+	print("Testing large resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
 def test_strip_white_largeres():
-	print("Testing white strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'Press-Red-White-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'LargeRes-Red-Press-White-Strip.png')
+
+	print("Testing large resolution white strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "WHITE"
 
+	print("Testing large resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
 def test_strip_yellow_largeres():
-	print("Testing yellow strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'Press-Red-Yellow-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'LargeRes-Red-Press-Yellow-Strip.png')
+
+	print("Testing large resolution yellow strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "YELLOW"
 
-def test_strip_other_largeres():
-	print("Testing other strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'Detonate-Blue-Red-Strip.png')
+	print("Testing large resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
+def test_strip_red_largeres():
+	img = cv2.imread(MODULE_IMAGES_DIR + 'LargeRes-Blue-Detonate-Red-Strip.png')
+
+	print("Testing large resolution red strip...")
 	strip_colour = button.extractStripColour(img)
-	assert strip_colour == "OTHER"
+	assert strip_colour == "RED"
+
+	print("Testing large resolution blue button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "BLUE"
+
+def test_strip_black_largres():
+	img = cv2.imread(MODULE_IMAGES_DIR + 'Yellow-Hold-Black-Strip.png')
+
+	print("Testing large resolution black strip...")
+	strip_colour = button.extractStripColour(img)
+	assert strip_colour == "UNKNOWN"
+
+	print("Testing large resolution yellow button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "YELLOW"
 
 
 ### SMALL RESOLUTION TESTS ###
 def test_strip_blue_smallres():
-	print("Testing blue strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Abort-Red-Blue-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Red-Abort-Blue-Strip.png')
+
+	print("Testing small resolution blue strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "BLUE"
 
+	print("Testing small resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
 def test_strip_white_smallres():
-	print("Testing white strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Abort-Red-White-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Red-Abort-White-Strip.png')
+
+	print("Testing small resolution white strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "WHITE"
 
+	print("Testing small resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
 def test_strip_yellow_smallres():
-	print("Testing yellow strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Abort-Red-Yellow-Strip.png')
+	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Red-Abort-Yellow-Strip.png')
+
+	print("Testing small resolution yellow strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "YELLOW"
 
-def test_strip_other_smallres():
-	print("Testing other strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Abort-Red-Red-Strip.png')
+	print("Testing small resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
+def test_strip_red_smallres():
+	img = cv2.imread(MODULE_IMAGES_DIR + 'SmallRes-Red-Abort-Red-Strip.png')
+
+	print("Testing small resolution red strip...")
 	strip_colour = button.extractStripColour(img)
-	assert strip_colour == "OTHER"
+	assert strip_colour == "RED"
+
+	print("Testing small resolution red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
 
 
 ### DARKER SHADES TESTS ###
 def test_strip_darkblue_largeres():
-	print("Testing dark blue strip...")
 	img = cv2.imread(MODULE_IMAGES_DIR + 'Blue-Abort-DarkBlue-Strip.png')
+
+	print("Testing dark blue strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "BLUE"
 
+	print("Testing blue button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "BLUE"
+
 def test_strip_darkwhite_largeres():
+	img = cv2.imread(MODULE_IMAGES_DIR + 'White-Detonate-Grey-Strip.png')
+
 	print("Testing dark white strip...")
-	img = cv2.imread(MODULE_IMAGES_DIR + 'Detonate-White-Grey-Strip.png')
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "WHITE"
 
+	print("Testing white button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "WHITE"
+
 def test_strip_darkyellow_largeres():
-	print("Testing dark yellow strip...")
 	img = cv2.imread(MODULE_IMAGES_DIR + 'Red-Detonate-DarkYellow-Strip.png')
+
+	print("Testing dark yellow strip...")
 	strip_colour = button.extractStripColour(img)
 	assert strip_colour == "YELLOW"
 
-def test_strip_darkother_largeres():
-	print("Testing dark other strip...")
+	print("Testing red button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "RED"
+
+def test_strip_darkred_largeres():
 	img = cv2.imread(MODULE_IMAGES_DIR + 'Blue-Abort-DarkRed-Strip.png')
+
+	print("Testing dark red strip...")
 	strip_colour = button.extractStripColour(img)
-	assert strip_colour == "OTHER"
+	assert strip_colour == "RED"
+
+	print("Testing blue button...")
+	button_colour = button.extractButtonColour(img)
+	assert button_colour == "BLUE"
 
 
 test_strip_blue_largeres()
 test_strip_white_largeres()
 test_strip_yellow_largeres()
-test_strip_other_largeres()
+test_strip_red_largeres()
+test_strip_black_largres()
 
 test_strip_blue_smallres()
 test_strip_white_smallres()
 test_strip_yellow_smallres()
-test_strip_other_smallres()
+test_strip_red_smallres()
 
 test_strip_darkblue_largeres()
 test_strip_darkwhite_largeres()
 test_strip_darkyellow_largeres()
-test_strip_darkother_largeres()
+test_strip_darkred_largeres()
